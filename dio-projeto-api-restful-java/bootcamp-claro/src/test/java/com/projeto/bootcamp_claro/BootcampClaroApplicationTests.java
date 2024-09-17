199e9e16d08043ed7b0ffb630780dedac2328fad
filipe <filipe.candido.rodrigues@gmail.com>
2024-09-16 23:11:18 -0300

    Renomeando a classe Aplication

---
+++ dio-projeto-api-restful-java/bootcamp-claro/src/test/java/com/projeto/bootcamp_claro/BootcampClaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.bootcamp_claro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampClaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
