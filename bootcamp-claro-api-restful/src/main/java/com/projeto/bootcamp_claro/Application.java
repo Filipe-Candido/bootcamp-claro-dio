308b49d518c4c1b507be4023b24af7699b3fd3bd
filipe <filipe.candido.rodrigues@gmail.com>
2024-09-17 00:15:51 -0300

    inicio

---
+++ bootcamp-claro-api-restful/src/main/java/com/projeto/bootcamp_claro/Application.java
@@ -0,0 +1,13 @@
+package com.projeto.bootcamp_claro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
