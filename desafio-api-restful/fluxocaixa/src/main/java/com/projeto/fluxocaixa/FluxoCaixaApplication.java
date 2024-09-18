eee95c3b5e18bd4de176b6144e9bacf6c3ddf042
filipe <filipe.candido.rodrigues@gmail.com>
2024-09-17 23:19:08 -0300

    Adicionando xml e renomeando classe

--- desafio-api-restful/fluxocaixa/src/main/java/com/projeto/fluxocaixa/FluxoCaixaApplication.java
+++ desafio-api-restful/fluxocaixa/src/main/java/com/projeto/fluxocaixa/FluxoCaixaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FluxocaixaApplication {
+public class FluxoCaixaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FluxocaixaApplication.class, args);
+		SpringApplication.run(FluxoCaixaApplication.class, args);
 	}
 
 }
